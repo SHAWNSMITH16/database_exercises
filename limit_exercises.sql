163c599fa5a1aa83793b23088bdf7dfea64026bd
Shawn Smith <Shawn.R.Smith16@gmail.com>
2022-10-25 12:33:46 -0500

    added completed limit_exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,43 @@
+/*
+Create a new SQL script named limit_exercises.sql.
+
+MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. For example, to find all the unique titles within the company, we could run the following query:
+
+
+SELECT DISTINCT title FROM titles;
+List the first 10 distinct last name sorted in descending order.
+
+Facello
+Simmel
+Bamford
+Koblick
+Maliniak
+Preusig
+Zielinski
+Kalloufi
+Peac
+Piveteau*/
+USE employees;
+SELECT DISTINCT last_name FROM employees
+LIMIT 10;
+
+/*
+Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.*/
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
+AND birth_date LIKE '%-12-25%' 
+ORDER BY hire_date ASC
+LIMIT 5; /* Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai, Kushner, Petter Stroustrup*/
+
+/*
+Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.*/
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
+AND birth_date LIKE '%-12-25%' 
+ORDER BY hire_date ASC
+LIMIT 5 OFFSET 45;
+
+/*
+LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+You would have to do a little bit of simple arithmetic to know how many pages to offset to get the result you want
+*/
